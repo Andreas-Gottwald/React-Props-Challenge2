0f846238d9eddd2a8269b2aa49a7df978924be41
Andreas Gottwald <andreas.gottwald@hotmail.de>
2023-12-27 13:40:06 +0100

    finish

--- src/App.js
+++ src/App.js
@@ -2,5 +2,13 @@ import React from "react";
 import "./styles.css";
 
 export default function App() {
-  return <h1>Replace me with your component!</h1>;
+  return <Sum ValueA={6} ValueB={8} />;
+}
+
+function Sum({ ValueA, ValueB }) {
+  return (
+    <h1>
+      The result of {ValueA} and {ValueB} is: {ValueA + ValueB}
+    </h1>
+  );
 }
